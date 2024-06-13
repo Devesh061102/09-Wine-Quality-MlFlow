4c7481824e3c5f16681f321e28b3a38a950f95b0
Devesh061102 <deveshnegi10nov@gmail.com>
2024-06-13 14:25:02 +0530

    Data Transformatiom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Wine_Quality_Project import logger
 from Wine_Quality_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Wine_Quality_Project.pipeline.stage_02_data_validation import DataValiadationTrainingPipeline
+from Wine_Quality_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -16,6 +17,17 @@ STAGE_NAME = "Data Validation Stage"
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataValiadationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
